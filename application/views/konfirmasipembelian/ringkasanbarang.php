101cb529fd1602ef67364a7b17a5f5ba8cd3b97e
mjaddid <47092091+mjaddid@users.noreply.github.com>
2019-04-23 23:46:22 +0700

    Add files via upload

--- application/views/konfirmasipembelian/ringkasanbarang.php
+++ application/views/konfirmasipembelian/ringkasanbarang.php
@@ -1,7 +1,7 @@
 &nbsp;
 <div class="container">
 	<div class="row">
-		<div class="card col-md-4 col-sm-4 text-center">
+		<div style="background: black;color: white;" class="card col-md-4 col-sm-4 text-center">
 			<p href="#" class=" card-title">1. Ringkasan Belanja</p>
 		</div>
 		<div class="card col-md-4 col-sm-4 text-center">
@@ -21,16 +21,18 @@
 		<th class="col-8"></th>
 	  </thead>
 	  <tbody>
+	  	<?php foreach ($tampilkandata as $show) { ?>
 		<tr>
-			  <td><img class="col-md-12 col-sm-12" src="<?php echo base_url('assets/produk/listproduk/JP1.jpg'); ?>"></td>
+			  <td><img class="col-md-12 col-sm-12" src="<?php echo base_url('assets/produk/listproduk/').$show->gambarproduk; ?>"></td>
 			  <td>
-				<p>Nama Produk <span class="btn btn-sm <?php echo $this->session->userdata('warnagender'); ?>" style="font-size:25px; float:right;">&times;</span></p>
-				<p>Rp<?php echo number_format(/*$p->harga*/928302,0,",","."); ?>,00</p>
-				<p>UKURAN | JUMLAH </p>
+				<p><?php echo $show->namaproduk ?> <span class="btn btn-sm <?php echo $this->session->userdata('warnagender'); ?>" style="font-size:25px; float:right;">&times;</span></p>
+				<p>Rp<?php echo number_format($show->harga,0,",","."); ?>,00</p>
+				<p><?php echo $show->ukuranproduk; ?> | <?php echo $show->jumlahproduk; ?> </p>
 				<hr>
-				<p class="text-right">Rp<?php echo number_format(/*$p->harga*/928302,0,",","."); ?>,00</p>
+				<p class="text-right">Rp<?php echo number_format($this->session->userdata('totalharga'),0,",","."); ?>,00</p>
 			  </td>
 		</tr>
+	    <?php } ?>
 	  </tbody>
 	</table>
 </div>
@@ -38,10 +40,10 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-4 col-sm-4">
-			<a href="#" class="btn <?php echo $this->session->userdata('warnagender'); ?> btn-fluid btn-block">KEMBALI</a>
+			<a href="<?php echo base_url('Geoffmax/index');?>" class="btn <?php echo $this->session->userdata('warnagender'); ?> btn-fluid btn-block">KEMBALI</a>
 		</div>
 		<div class="col-md-8 col-sm-8">
-			<a href="#" class="btn <?php echo $this->session->userdata('warnagender'); ?> btn-block">LANJUT KE PEMBAYARAN</a>
+			<a href="<?php echo base_url('Geoffmax/identitasdatadiri'); ?>" class="btn <?php echo $this->session->userdata('warnagender'); ?> btn-block">LANJUT KE PEMBAYARAN</a>
 		</div>
 	</div>
 </div>
